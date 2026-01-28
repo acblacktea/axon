d770c7b56347f63a6b2f97a9ff69a83a03a15818
acblacktea <acblacktea@outlook.com>
2026-01-28 23:19:47 +0800

    update

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calais_order_execution",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.9.0",
+        "websockets>=12.0",
+        "pyyaml>=6.0",
+    ],
+    python_requires=">=3.10",
+    author="Yang Liu",
+    author_email="acblacktea@outlook.com",
+    description="Calais order execution service for cryptocurrency options trading",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
